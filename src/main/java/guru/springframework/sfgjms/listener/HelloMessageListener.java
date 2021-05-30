5133290d750b304f2fb4b01fe7a4870e4a5aaa6f
NashidForbes <NashidForbes@biohumans.com>
2021-05-29 20:15:13 -0400

    This commit using local ActiveMQ Broker (with Docker) with Spring Boot.

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -2,25 +2,44 @@ package guru.springframework.sfgjms.listener;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
-
+import lombok.AllArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
+@AllArgsConstructor
 @Component
 public class HelloMessageListener {
 
+    private final JmsTemplate jmsTemplate;
+
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
-                       @Headers MessageHeaders headers, Message message){
+                       @Headers MessageHeaders headers, Message message) {
+/*        System.out.println("I got a message.");
+        System.out.println(helloWorldMessage);*/
+    }
 
-        System.out.println("I got a message.");
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                               @Headers MessageHeaders headers, Message message)
+            throws JMSException {
 
-        System.out.println(helloWorldMessage);
+        HelloWorldMessage payLoadMsg = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Sent and Recieved Hello World!")
+                .build();
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payLoadMsg);
+
+        System.out.println(helloWorldMessage.toString());
     }
 
 }
